1b80caf5c62c186b8c08490b91ce781090e9dd5b
Kamen Kirilov <kamentrg91@gmail.com>
2018-06-04 13:31:39 +0300

    end of first video commit!

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-   res.render("landing");
+app.get("/", (req, res) => res.render("landing") );
+
+app.get("/campgrounds", (req, res) => {
+   let campgrounds = [
+      {name: "Bozveliisko", image: "https://farm2.staticflickr.com/1266/4701470104_07556d68d6.jpg"},
+      {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"},
+      {name: "Mountain Pirin", image: "https://farm4.staticflickr.com/3053/2586934044_339a678e73.jpg"}
+   ]
+
+   res.render("campgrounds", {campgrounds: campgrounds});
 });
-
-
 
 
 app.listen(3000, () => console.log("The YelpCamp Server is started on port 3000!") );
